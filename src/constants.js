674f55efc800697cfcd6fd0d04f0e7ae0ec4807b
Amrita3292 <167297651+Amrita3292@users.noreply.github.com>
2025-12-10 23:51:37 +0530

    Fix merge conflict in constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,7 @@
 const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
-<<<<<<< HEAD
 export const URL = `https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
 
 
-=======
-export const URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
->>>>>>> 55b09b0a4cb734491ff106c9d1e268cb7d3e740a
+// =======
+// export const URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
+
